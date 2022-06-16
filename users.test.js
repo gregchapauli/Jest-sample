1ef8950e3294eb559f168984473d2c14b49dfb62
Gregory Caloin <gregchapauli@gmail.com>
2022-06-16 15:27:02 +0200

    Création d'un test avec mock de axios

--- users.test.js
+++ users.test.js
@@ -1,2 +1,12 @@
 import Users from "./user";
 import axios from "axios";
+
+jest.mock("axios");
+
+test("doit récupérer les users de l'api", () => {
+  const users = [{ name: "mike" }, { name: "mike" }];
+  const resp = { data: users };
+  axios.get.mockResolvedValue(resp);
+
+  return Users.all().then((data) => expect(data).toEqual(users));
+});
